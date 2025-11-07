a3d9046dc3c381f74768c2ac65ecc6c472bcec94
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-11-07 23:14:34 +0000

    Starting routes implementation

--- eater-app/src/App.jsx
+++ eater-app/src/App.jsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>,
+  },
+  {
+    path: "/cart",
+    element: <div>Cart Page</div>,
+  },
+  {
+    path: "/checkout",
+    element: <div>Checkout Page</div>,
+  }
+]);
+
 export default function App() {
-  return(
-    <></>
-  );
+  return <RouterProvider router={router} />;
 }
