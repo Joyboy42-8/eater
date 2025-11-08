c77d6bc77aa966727a6867ef6b95d01311d080d9
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-11-08 18:18:20 +0000

    Setting Images

--- eater-app/src/pages/Home.jsx
+++ eater-app/src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Hero() {
             animate={{ scale: 1 }} 
             whileTap={{ scale: 1.1, transition: { duration: 1 } }}
         >
-            <img src="https://i.pinimg.com/1200x/a2/da/d9/a2dad9ca2e0d9ca51c28827e671cb146.jpg" alt="Burger One" className="HeroImage" />
+            <img src="" alt="Burger One" className="HeroImage" />
             <p className="HeroText">Smoky Cheesy Burger</p>
         </motion.section>
     );
