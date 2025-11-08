4159f99b072f2fd8e687e474a48d922bdd5e0e2a
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-11-08 18:00:41 +0000

    settings images

--- eater-app/src/pages/Home.jsx
+++ eater-app/src/pages/Home.jsx
@@ -3,19 +3,20 @@ import { motion } from "framer-motion";
 export default function Home() {
     return (
         <main className="Main">
-            <motion.h1 className="Title" 
-            initial={{ scale: 0 }} 
-            animate={{ scale: 1 }} 
-            whileTap={{ scale: 1.1, transition: { duration: 1 } }}
-            >
-                Home Page
-            </motion.h1>
+            <Hero />
         </main>
     );
 }
 
 function Hero() {
     return(
-        <section></section>
+        <motion.section className="Hero" 
+            initial={{ scale: 0 }} 
+            animate={{ scale: 1 }} 
+            whileTap={{ scale: 1.1, transition: { duration: 1 } }}
+        >
+            <img src="https://i.pinimg.com/1200x/a2/da/d9/a2dad9ca2e0d9ca51c28827e671cb146.jpg" alt="Burger One" className="HeroImage" />
+            <p className="HeroText">Smoky Cheesy Burger</p>
+        </motion.section>
     );
 }
